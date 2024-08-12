e8e2d27926e798d7026e4243f33e89a86531d57d
Vinicius Faria <artixsproductions@gmail.com>
2024-08-12 12:13:07 -0300

    refac: feito com que os datasets sejam apenas testados, e nao adicionados, evitando desperdicio de memoria

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def read_all_synthetic(toy, dir):
     
     for file in files:
         p,l,k = read_custom_synthetic(f"{dir}/{file}")
-        toy.add_dataset(p,l,k,file.split(".")[0])
+        toy.test_dataset(p,l,k,file.split(".")[0])
 
 def read_all_real(toy, dir):
     'Reads all instances in directory and add them as dataset in toy'
@@ -23,44 +23,44 @@ def read_all_real(toy, dir):
     
     for file in files:
         p,l,k = read_real(f"{dir}/{file}")
-        toy.add_dataset(p,l,k,file.split(".")[0])
+        toy.test_dataset(p,l,k,file.split(".")[0])
 
 def gen_scikit_datasets(toy, n_samples = 500, seed = 30):
     'Adds scikit datasets to toy'
-    toy.add_dataset(*datasets.make_moons(n_samples=n_samples, noise=0.05, random_state=seed),
+    toy.test_dataset(*datasets.make_moons(n_samples=n_samples, noise=0.05, random_state=seed),
                     2, "moons")
 
-    toy.add_dataset(*datasets.make_blobs(n_samples=n_samples, random_state=seed), 
+    toy.test_dataset(*datasets.make_blobs(n_samples=n_samples, random_state=seed), 
                     3, "blobs")
 
-    toy.add_dataset(*datasets.make_circles(n_samples=500, noise=0.05, random_state=seed), 2, "circles")
+    toy.test_dataset(*datasets.make_circles(n_samples=500, noise=0.05, random_state=seed), 2, "circles")
 
     #Clusters de dados em dimensao maior -> Parecido com o blobs, mas em alta dimensao
 
     #Varios valores informativos
-    toy.add_dataset(*datasets.make_classification(n_samples, n_redundant=0, n_informative=10, n_features=10, random_state=seed), 2, "high-order-informative")
+    toy.test_dataset(*datasets.make_classification(n_samples, n_redundant=0, n_informative=10, n_features=10, random_state=seed), 2, "high-order-informative")
 
     #Alguns valores redundantes
-    toy.add_dataset(*datasets.make_classification(n_samples, n_redundant=3, n_informative=7, n_features=10,random_state=seed), 2, "ho-slightly-redundant")
+    toy.test_dataset(*datasets.make_classification(n_samples, n_redundant=3, n_informative=7, n_features=10,random_state=seed), 2, "ho-slightly-redundant")
 
     #Grande parte dos valores redundantes
-    toy.add_dataset(*datasets.make_classification(n_samples, n_redundant=7, n_informative=3, n_features=10,random_state=seed), 2, "ho-redundant")
+    toy.test_dataset(*datasets.make_classification(n_samples, n_redundant=7, n_informative=3, n_features=10,random_state=seed), 2, "ho-redundant")
 
     #Grande parte dos valores sendo inuteis
-    toy.add_dataset(*datasets.make_classification(n_samples, n_redundant=0, n_informative=4, n_features=10,random_state=seed), 2, "ho-useless")
+    toy.test_dataset(*datasets.make_classification(n_samples, n_redundant=0, n_informative=4, n_features=10,random_state=seed), 2, "ho-useless")
 
     #Caso geral - numero razoavel de inuteis, repetidos, redundantes e informativos
-    toy.add_dataset(*datasets.make_classification(n_samples, n_redundant=2, n_repeated=2 ,n_informative=5, n_features=10,random_state=seed), 2, "ho-general")
+    toy.test_dataset(*datasets.make_classification(n_samples, n_redundant=2, n_repeated=2 ,n_informative=5, n_features=10,random_state=seed), 2, "ho-general")
 
     #Anisotropic
     p, l = datasets.make_blobs(n_samples=n_samples, random_state=seed)
 
     transformation = [[0.6, -0.6], [-0.4, 0.8]]
     p = np.dot(p, transformation)
-    toy.add_dataset(p,l,3,"aniso")
+    toy.test_dataset(p,l,3,"aniso")
 
     rng = np.random.RandomState(seed)
-    toy.add_dataset(rng.rand(n_samples,2), rng.randint(2, size=n_samples), 2, "random")
+    toy.test_dataset(rng.rand(n_samples,2), rng.randint(2, size=n_samples), 2, "random")
 
 def main():
     toy = ToyDatasets("./results/toy.csv", "./img")
--- toydatasets.py
+++ toydatasets.py
@@ -1,18 +1,18 @@
 from alg import Instance
-from util import minkowski_distance
 from sklearn.metrics import silhouette_score, adjusted_rand_score
 from time import time
-
 import numpy as np
 import matplotlib.pyplot as plt
 from util import build_dist_matrix
+import multiprocessing as mp
 
 class ToyDatasets():
     def __init__(self, csv_output_file, img_folder):
-        self.data_points = []
-        self.data_labels = []
-        self.instances = []
-        self.names = []
+        self.curr_points = []
+        self.curr_labels = []
+        self.curr_instance = None
+        self.curr_name = ''
+        self.pool = mp.Pool()
         
         self.csv_output_file = csv_output_file
         self.img_folder = img_folder
@@ -21,33 +21,35 @@ class ToyDatasets():
         with open(csv_output_file, 'w') as f:
             f.write("instance,radius,silhouette,adj_rand_score,exec_time,method,p\n")
 
-    def add_dataset(self, points, labels, k, name, p=2):
-        'Adds a sklearn toy dataset and adds a corresponding instance'
+    def test_dataset(self, points, labels, k, name, p=2, itr=30):
+        'Creates an instance of a dataset and runs it for testing'
 
         dist = self.make_dist_matrix(points, p)
-        #Make labels
+        #Make point names
         S = list(range(len(points)))
         inst =  Instance(S, k, points, p, dist)
 
-        self.data_points.append(points)
-        self.data_labels.append(labels)
-        self.instances.append(inst)
-        self.names.append(name)
+        self.curr_points = points
+        self.curr_labels = labels
+        self.curr_instance = inst
+        self.curr_name = name
 
-        self.plot_dataset_img(len(self.instances) - 1)
+        self.plot_dataset_img()
+
+        self.test_curr_dataset(itr)
 
     def make_dist_matrix(self, points, p):
         'Returns a distance matrix based on dataset points and p value for minkowski distance'
 
         return build_dist_matrix(points, p)
 
-    def plot_dataset_img(self, idx, labels=None, img_name='', circles=None, title=''):
-        'Plots the dataset in index idx as a .pdf file with path img_folder/img_name'
+    def plot_dataset_img(self, labels=None, img_name='', circles=None, title=''):
+        'Plots the current dataset as a .pdf file with path img_folder/img_name'
 
         fig, ax = plt.subplots()
 
         if labels is None:
-            labels = self.data_labels[idx]
+            labels = self.curr_labels
 
         set_labels = set(labels)
         label_to_color = {label: i for i, label in enumerate(set_labels)}
@@ -55,8 +57,8 @@ class ToyDatasets():
         # Map the labels to color indices
         color_indices = [label_to_color[label] for label in labels]
 
-        X = [x[0] for x in self.data_points[idx]]
-        Y = [x[1] for x in self.data_points[idx]]
+        X = [x[0] for x in self.curr_points]
+        Y = [x[1] for x in self.curr_points]
 
         cmap = plt.get_cmap('tab20', len(set_labels))
 
@@ -71,7 +73,7 @@ class ToyDatasets():
 
         ax.set_aspect('equal', 'box')
         fig.suptitle(title)
-        plt.savefig(f"{self.img_folder}/{self.names[idx] if img_name == '' else img_name}.pdf", format='pdf')
+        plt.savefig(f"{self.img_folder}/{self.curr_name if img_name == '' else img_name}.pdf", format='pdf')
         plt.close()
 
     def time_execution(self, fn, *args):
@@ -82,32 +84,32 @@ class ToyDatasets():
                 
         return res, exec_time
 
-    def test_datasets(self, itr=30):
-        'Test all datasets for itr iterations'
-        for idx, (inst, name) in enumerate(zip(self.instances, self.names)):
-            for method, method_name in zip([inst.scikit_k_clusters, inst.k_clusters, inst.refining_k_clusters], ["scikit", "greedy", "refining"]):
-                best_C, best_labels, radiuses, best_r = None, None, None, np.inf
-                for _ in range(itr):
-                    (C, labels, radius), exec_time = self.time_execution(method)
+    def test_curr_dataset(self, itr=30):
+        'Test current dataset for itr iterations'
+        inst = self.curr_instance
+        for method, method_name in zip([inst.scikit_k_clusters, inst.k_clusters, inst.refining_k_clusters], ["scikit", "greedy", "refining"]):
+            best_C, best_labels, radiuses, best_r = None, None, None, np.inf
+            for _ in range(itr):
+                (C, labels, radius), exec_time = self.time_execution(method)
 
-                    max_r = max(radius)
-                    if max_r < best_r:
-                        best_C, best_labels, radiuses, best_r = C, labels, radius, max_r
+                max_r = max(radius)
+                if max_r < best_r:
+                    best_C, best_labels, radiuses, best_r = C, labels, radius, max_r
 
-                    #Write results
-                    self.write_results(idx, C, labels, max_r, exec_time, method_name, inst.p)
-                
-                #Plot best result
-                #Calculate associated circles
-                circles = list(zip(best_C, radiuses))
-                self.plot_dataset_img(idx, best_labels, img_name=name + method_name, 
-                                        circles=circles, title=f"Max radius: {best_r}")
+                #Write results
+                self.write_results(self.curr_name, C, labels, max_r, exec_time, method_name, inst.p)
+            
+            #Plot best result
+            #Calculate associated circles
+            circles = list(zip(best_C, radiuses))
+            self.plot_dataset_img(best_labels, img_name=self.curr_name + method_name, 
+                                    circles=circles, title=f"Max radius: {best_r}")
 
-    def write_results(self, idx, C, labels, radius, exec_time, method, p):
+    def write_results(self, name, C, labels, radius, exec_time, method, p):
         'Writes relevant results for the csv'
 
-        sil = silhouette_score(self.instances[idx].dist, labels)
-        adjrand = adjusted_rand_score(self.data_labels[idx],labels)
+        sil = silhouette_score(self.curr_instance.dist, labels)
+        adjrand = adjusted_rand_score(self.curr_labels,labels)
         
         with open(self.csv_output_file, 'a') as f:
-            f.write(f"{self.names[idx]},{float(radius)},{float(sil)},{float(adjrand)},{exec_time},{method},{p}\n")
+            f.write(f"{name},{float(radius)},{float(sil)},{float(adjrand)},{exec_time},{method},{p}\n")
